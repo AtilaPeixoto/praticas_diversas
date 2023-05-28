b4f9caa6098784536a8454503a701b6838ff62ba
AtilaPeixoto <atila.yuri.peixoto@gmail.com>
2023-05-28 16:25:25 -0300

    incorporação de thema e alinhamento de labels

--- Tkinter/NumberGame/Advinhe_numero.py
+++ Tkinter/NumberGame/Advinhe_numero.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import ttk
 from sub_frame import *
 from sub_frame2 import *
 
@@ -15,8 +15,9 @@ contador = Contador(janela)
 contador.grid(row=0, column=0, columnspan=2)
 
 
+style = ttk.Style()
 
-sub_1 = Jogando(janela, contador)
+sub_1 = Jogando(janela, contador, style)
 sub_1.grid(row=1, column=0, rowspan=4, columnspan=4, padx=50, pady=50, )
 
 janela.mainloop()
--- Tkinter/NumberGame/sub_frame.py
+++ Tkinter/NumberGame/sub_frame.py
@@ -1,38 +1,52 @@
-from tkinter import *
+from tkinter import ttk
 from random import randint
 from sub_frame2 import *
 
 
 class Jogando(Frame):
-    def __init__(self, x, contador):
+    def __init__(self, x, contador, style):
         super().__init__()
         self.contador = contador 
+        
+        self.s = style
+        self.tema = self.s.theme_use('clam')
+        self.fundo = self.s.lookup('self.tema', 'background')
+        
         self['bd'] = 2
         self['relief'] = 'solid'
+        self['background'] = self.fundo
         
-        borda_frame = Frame(self)
+        borda_frame = Frame(self, height=200, width=300,)
+        borda_frame['background'] = self.fundo
+        borda_frame.grid_propagate(0)
+        
         
         self.numero_jogador = StringVar()   
         self.falar = StringVar()
         self.n_bot = 0
-        self.lista_maior_menor = ['Muito baixo, tente um maior',
-                                  'Muito Alto, tente um menor.',
-                                  'Acertou!!!',
-                                  'Isso não é um numero.\nDigite um numero!',
-                                  ]
+        self.lista_maior_menor = [
+                                'Muito baixo, tente um maior.',
+                                'Muito Alto, tente um menor.',
+                                'Acertou!!!',
+                                'Isso não é um numero.\nDigite um numero!',
+                                ]
         
-        lbl = Label(borda_frame, text='Escolha um número! \nTente a sua sorte!!!')
-        self.campo_numero = Entry(borda_frame, textvariable=self.numero_jogador)
-        self.btn = Button(borda_frame, text='Jogar', command=self.executar)
-        lbl_resposta = Label(borda_frame, textvariable=self.falar, width=25)
         
-        lbl.grid()
-        self.campo_numero.grid()
+        lbl = ttk.Label(borda_frame, text='Escolha um número! \nTente a sua sorte!!!')
+        self.campo_numero = ttk.Entry(borda_frame, textvariable=self.numero_jogador)
+        self.btn = ttk.Button(borda_frame, text='Jogar', command=self.executar)
+        lbl_resposta = ttk.Label(borda_frame, textvariable=self.falar, anchor='center', justify='center', width=30)
+        
+       
+        
+        lbl.grid(row=1, column=1, padx=70, pady=10)
+        self.campo_numero.grid(row=2, column=1, padx=70, pady=10)
         self.campo_numero.bind('<Return>', lambda event:self.executar())
-        self.btn.grid()
-        lbl_resposta.grid()
         self.campo_numero.focus()
-        borda_frame.grid(padx=100, pady=30)
+        
+        self.btn.grid(row=3, column=1, pady=10, padx=70)
+        lbl_resposta.grid(row=4, column=1, padx=70)
+        borda_frame.grid(padx=50, pady=5, ipady=5, ipadx=10)
         
         
     def validacao(self):
@@ -74,5 +88,7 @@ class Jogando(Frame):
         
     def pegar_numero(self):
         self.contador.contar()
-        return int(self.numero_jogador.get())    
+        return int(self.numero_jogador.get())  
+    
+  
         
