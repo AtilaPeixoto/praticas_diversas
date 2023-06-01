07af2fb629653a8ccc482b398f80d1557bf6f722
AtilaPeixoto <atila.yuri.peixoto@gmail.com>
2023-06-01 13:27:56 -0300

    adicionando o readme

---
+++ Tkinter/QR_Gerador/README.md
@@ -0,0 +1,14 @@
+# gerador e QRcode
+
+Foi criado para a pratica da criação da interface.
+* implemetei de um texto fantasma dentro das caixas de dialogo
+* trablhei com ttk.Style para suavisar a interface como um todo
+* grid foi usado para alinhamento dos widgets
+* icone
+* imagem como marca posicionada no grid
+* modularização como método de organazação
+
+E também faz cria QrCodes ;)
+
+Há um bug que ainda nao fui capaz de resolver, ele aparece quando implementei o texto fantasma.
+Trata-se de um aumento da fonte e da caixa de dialogo quando chamada on_entry_click.
--- Tkinter/QR_Gerador/frame1.py
+++ Tkinter/QR_Gerador/frame1.py
@@ -10,6 +10,8 @@ class Gerador(Frame):
         super().__init__()
         self.item = StringVar()
         self.nomeItem = StringVar()
+        self['bg'] = 'black'
+    
         self.style = style
         self.style.theme_use('vista')
         
@@ -19,13 +21,13 @@ class Gerador(Frame):
         
         self.instrucao1 = ttk.Label(self, text='Escreva algo ou cole um link:', border=1, relief='sunken')
         self.instrucao2 = ttk.Label(self, text='Escreva o nome do arquivo:', border=1, relief='sunken')
-        self.entrada1 = ttk.Entry(self, textvariable=self.item)
+        self.entrada1 = ttk.Entry(self, textvariable=self.item, )
         self.entrada2 = ttk.Entry(self, textvariable=self.nomeItem)
         self.executar = ttk.Button(self, text='Gerar QRcode', command=lambda:recolher_itens(self.item, self.nomeItem))
         
         self.instrucao1.grid(row=0, column=0, padx=10, pady=10)
         self.instrucao2.grid(row=1, column=0, padx=10, pady=10)
-        self.entrada1.grid(row=0, column=1, padx=10, pady=10)
+        self.entrada1.grid(row=0, column=1, padx=20, pady=20)
         self.entrada2.grid(row=1, column=1, padx=10, pady=10)
         self.executar.grid(row=2, column=0, padx=10, pady=10)
         marca.grid(row=2, column=1, padx=10, pady=10,)
@@ -34,6 +36,7 @@ class Gerador(Frame):
         self.entrada1.insert(0, 'Digite algo aqui...')
         self.entrada1.bind('<FocusIn>', self.on_entry_click)
         self.entrada1.bind('<FocusOut>', self.on_focusout)
+        self.entrada1.grid_propagate(0)
                 
         def recolher_itens(item, nomeItem):
             self.item.set(self.entrada1.get())
@@ -44,11 +47,11 @@ class Gerador(Frame):
     def on_entry_click(self, event):
         if self.entrada1.get() == 'Digite algo aqui...':
            self.entrada1.delete(0, END)
-           self.entrada1.config(font='black 12')
+           self.entrada1.config(font='black ')
 
     def on_focusout(self, event):
         if self.entrada1.get() == '':
            self.entrada1.insert(0, 'Digite algo aqui...')
-           self.entrada1.config(font='grey 12')
+           self.entrada1.config(font='grey ')
         
   
