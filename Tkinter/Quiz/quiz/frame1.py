ec033ea615d23d180555502d4dcd3a81c0230384
AtilaPeixoto <atila.yuri.peixoto@gmail.com>
2023-06-04 15:57:37 -0300

    verificação

--- Tkinter/Quiz/quiz/frame1.py
+++ Tkinter/Quiz/quiz/frame1.py
@@ -4,8 +4,8 @@ import random
 
 
 
-perguntas = [['Qual é a capital do Brasil?', 'Brasília', ['Rio de Janeiro', 'São Paulo', 'Belo Horizonte']],
-                ['Qual é o maior animal terrestre?', 'Elefante', ['Girafa', 'Leão', 'Hipopótamo']]
+perguntas1 = [['Qual é a capital do Brasil?', 'Brasília',  ['Brasília', 'Rio de Janeiro', 'São Paulo', 'Belo Horizonte']],
+                ['Qual é o maior animal terrestre?', 'Elefante', ['Elefante', 'Girafa', 'Leão', 'Hipopótamo']]
     ]
 
 class Jogo(Frame):
@@ -17,15 +17,16 @@ class Jogo(Frame):
         self.inicio = ttk.Button(self, text='START', command=self.start)
         self.inicio.grid()
         
-        frame_borda = Frame(self)
-        self.pergunta = ttk.Label(frame_borda, text='Pergunta')
+        frame_borda = Frame(self, height=100, width=200)
+        self.lbl_pergunta = ttk.Label(frame_borda, text='')
         
         frame_borda.grid(padx=50, pady=25, ipady=10, ipadx=5)
-        self.pergunta.grid(row=0)
+        self.lbl_pergunta.grid(row=0)
+        frame_borda.grid_propagate(0)
         
         self.lista_btn = []    
         for i in range(1, 5):
-            btn_respostas = ttk.Button(frame_borda, text='respostas', command=lambda index=i: self.verificar(index))
+            btn_respostas = ttk.Button(frame_borda, text='respostas', width=30, command=lambda index=i: self.verificar(index))
             self.lista_btn.append(btn_respostas)
             btn_respostas.grid(row=i)
             
@@ -33,24 +34,31 @@ class Jogo(Frame):
             
             
     def verificar(self, index):
-        self.indice = 1
-        if self.indice == index:
+        print(index, self.indice)
+        if index == self.indice:
             self.msg = 'Parabens'
+            print('parabens')
         else:
             self.msg = 'Errado. Desejo mais sorte'
-        for i in range(4):
-            self.lista_btn[i].config(text='vazio')
+            print('errado')
+      
        
         
             
     def start(self):
-        sorteio = random.choice(perguntas)
-        self.pergunta = sorteio[0]
-        self.certa = sorteio[1]
-        self.opcoes = sorteio[2]
-        self.opcoes.insert(random.randint(0, 2), self.certa)
+        self.sorteio = random.choice(perguntas1)
+        self.lbl_pergunta.config(text=self.sorteio[0])
+        self.certa = self.sorteio[1]
+        self.opcoes = self.sorteio[2]
+        print(self.opcoes)
+        random.shuffle(self.opcoes)
+        self.indice = self.opcoes.index(self.certa) +1
         for i in range(4):
-            self.lista_btn[i].config(text=self.opcoes[i])
-            
+            self.lista_btn[i].config(text=self.opcoes[i] )
+      
  
+            
+    def zerar(self):
+        for i in range(4):
+            self.lista_btn[i].config(text='vazio')
             
